102929f78411090f68ba33036f9707fb4a0be9ef
Dandelo <github@p.uru.ch>
2013-12-14 18:55:22 +0100

    removed insecure and allowed certain writers to post.

--- collections/offers.js
+++ collections/offers.js
@@ -2,3 +2,9 @@
  * Created by mindestens on 12/13/13.
  */
 Offers = new Meteor.Collection('offers');
+Offers.allow({
+  insert: function(userId, doc) {
+    // only allow posting if you are logged in
+    return !!userId;
+  }
+});
